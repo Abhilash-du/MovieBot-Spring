9916b03a81ac8035fa78d86ca5b9f5410f6f7a6c
abhilash.dubey <abhilash.dubey@perforce.com>
2025-03-10 09:53:08 +0530

    [Lesson2]: Decoupling Components

--- src/main/java/io/datajek/spring/basics/movie_recommender_system/MovieRecommenderSystemApplication.java
+++ src/main/java/io/datajek/spring/basics/movie_recommender_system/MovieRecommenderSystemApplication.java
@@ -1,21 +1,14 @@
 package io.datajek.spring.basics.movie_recommender_system;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lesson1.RecommenderImplementation;
 
 @SpringBootApplication
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieRecommenderSystemApplication.class, args);
-		RecommenderImplementation recommender = new RecommenderImplementation();
-		String[] result = recommender.recommendMovies("Finding Dory");
-		// Printing recommended result
-		System.out.println(Arrays.toString(result));
 	}
 
 }
--- src/main/java/lesson1/ContentBasedFilter.java
+++
@@ -1,9 +0,0 @@
-package lesson1;
-
-public class ContentBasedFilter {
-	public String[] getRecommendation(String movie) {
-		// Logic of content based filter
-		return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
-	}
-
-}
